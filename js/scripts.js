739fa5b9f249a29d453d94c870eb3af4cb4dfee8
Max Osborne <BurntAluminum@gmail.com>
2019-01-30 09:50:31 -0800

    I should be ashamed of this.

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+$("form#primary")
+  var addstr = function(addone) {
+    return placeholder;
+  };
+
+  var addcon = function(addone) {
+    return placeholder;
+  };
+
+  var addper = function(addone) {
+    return placeholder;
+  }
+
+  var adddex = function(addone) {
+    return placeholder;
+  }
+
+  var addint = function(addone) {
+    return placeholder;
+  }
+
+  var addcha = function(addone) {
+    return placeholder;
+  }
+
+  var addwil = function(addone) {
+    return placeholder;
+  }
+
+  var addlck = function(addone) {
+    return placeholder;
+  }
+
+  var substr = function(subone) {
+    return placeholder;
+  }
+
+  var subcon = function(subone) {
+    return placeholder;
+  }
+
+  var subper = function(subone) {
+    return placeholder;
+  }
+
+  var dwndex = function(subone) {
+    return placeholder;
+  }
+
+  var dwnint = function(subone) {
+    return placeholder;
+  }
+
+  var dwncha = function(subone) {
+    return placeholder;
+  }
+
+  var dwnwil = function(subone) {
+    return placeholder;
+  }
+
+  var dwnlck = function(subone) {
+    return placeholder;
+  }
+  $("#outputstr").text(resultstr);
+  $("#outputcon").text(resultcon);
+  $("#outputper").text(resultper);
+  $("#outputdex").text(resultdex);
+  $("#outputint").text(resultint);
+  $("#outputcha").text(resultcha);
+  $("#outputwil").text(resultwil);
+  $("#outputlck").text(resultlck);
+};
