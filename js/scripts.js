477da59de51a711f7d3cc4f4f007a00a8e93a2d3
Max Osborne <BurntAluminum@gmail.com>
2019-02-21 14:01:13 -0800

    2/21/2018 This commit I finished making Derived Attributes function, and added a dropdown for Traits, future updates will include functional traits with descriptions, Age modifiers, and the ability to hover over an attribute/skill to read a description of how it functions.

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,18 @@ var int=1;
 var cha=1;
 var wil=1;
 var lck=1;
-var hp= function(str, con) {
-  return (str / 2) + (con * 3);
-}
+
+var hp = ((str * .5) + con) * 3;
+$("#hp").text(hp);
+var stn = (wil * .5) + con;
+var mv = dex * 1.5;
+var ac = (con * 10 / 12) + 5;
+var dr = (con + wil) + str * .5 +5;
+var pr = (con + wil) + str * .5 +5;
+var mb = str * .25;
+var hc = per * .25 +10;
+var cc = lck * .25 +10;
+var ap = dex * 1.25;
 
 function add (number) {
   if (number < 12) {
@@ -28,58 +37,136 @@ else {
   }
 }
 
+function calcHP() {
+  return Math.ceil(((str * .5) + con) * 3);
+}
+
+function calcSTN() {
+  return Math.ceil((wil * .5) + con);
+}
+
+function calcMV() {
+  return Math.ceil(dex * 1.5);
+}
+
+function calcAC() {
+  return Math.ceil((con * 10 / 12) + 5);
+}
+
+function calcDR() {
+  return Math.ceil((con + wil) + str * .5 +5);
+}
+
+function calcPR() {
+  return Math.ceil((con + wil) + str * .5 +5);
+}
+
+function calcMB() {
+  return Math.ceil(str * .25);
+}
+
+function calcHC() {
+  return Math.ceil(per * .25 +10);
+}
+
+function calcCC() {
+  return Math.ceil(lck * .25 +10);
+}
+
+function calcAP() {
+  return Math.ceil(dex * 1.25)
+}
+
 $(document).ready(function() {
 
   $(".strup").click(function() {
     str = add(str);
-    console.log(str);
-    hp()
+    hp = calcHP();
+    dr = calcDR();
+    pr = calcPR();
+    mb = calcMB();
     $("#str").text(str);
+    $("#hp").text(hp);
+    $("#dr").text(dr);
+    $("#pr").text(pr);
+    $("#mb").text(mb)
   });
 
   $(".strdn").click(function() {
     str = subtract(str);
-    console.log(str);
-    hp()
+    hp = calcHP();
+    dr = calcDR();
+    pr = calcPR();
+    mb = calcMB();
     $("#str").text(str);
+    $("#hp").text(hp);
+    $("#dr").text(dr);
+    $("#pr").text(pr);
+    $("#mb").text(mb);
   });
 
   $(".conup").click(function() {
     con = add(con);
-    console.log(con);
-    hp()
+    hp = calcHP();
+    ac = calcAC();
+    stn = calcSTN();
+    dr = calcDR();
+    pr = calcPR();
     $("#con").text(con);
+    $("#hp").text(hp);
+    $("#stn").text(stn);
+    $("#ac").text(ac);
+    $("#dr").text(dr);
+    $("#pr").text(pr);
   });
 
   $(".condn").click(function() {
     con = subtract(con);
-    console.log(con);
-    hp()
+    hp = calcHP();
+    ac = calcAC();
+    stn = calcSTN();
+    dr = calcDR();
+    pr = calcPR();
     $("#con").text(con);
+    $("#hp").text(hp);
+    $("#stn").text(stn);
+    $("#ac").text(ac);
+    $("#dr").text(dr);
+    $("#pr").text(pr);
   });
 
   $(".perup").click(function() {
     per = add(per);
     console.log(per);
+    hc = calcHC();
     $("#per").text(per);
+    $("#hc").text(hc);
   });
 
   $(".perdn").click(function() {
     per = subtract(per);
     console.log(per);
+    hc = calcHC();
     $("#per").text(per);
+    $("#hc").text(hc);
   });
 
   $(".dexup").click(function() {
     dex = add(dex);
-    console.log(dex);
+    mv = calcMV();
+    ap = calcAP();
     $("#dex").text(dex);
+    $("#mv").text(mv);
+    $("#ap").text(ap)
   });
 
   $(".dexdn").click(function() {
     dex = subtract(dex);
-    console.log(dex);
+    mv = calcMV();
+    ap = calcAP
     $("#dex").text(dex);
+    $("#mv").text(mv);
+    $("#ap").text(ap)
   });
 
   $(".intup").click(function() {
@@ -109,24 +196,40 @@ $(document).ready(function() {
   $(".wilup").click(function() {
     wil = add(wil);
     console.log(wil);
+    stn = calcSTN();
+    dr = calcDR();
+    pr = calcPR();
     $("#wil").text(wil);
+    $("#stn").text(stn);
+    $("#dr").text(dr);
+    $("#pr").text(dr);
   })
 
   $(".wildn").click(function() {
     wil = subtract(wil);
     console.log(wil);
+    stn = calcSTN();
+    dr = calcDR();
+    pr = caclPR();
     $("#wil").text(wil);
+    $("#stn").text(stn);
+    $("#dr").text(dr);
+    $("#pr").text(dr);
   })
 
   $(".lckup").click(function() {
     lck = add(lck);
     console.log(lck);
+    cc = calcCC();
     $("#lck").text(lck);
+    $("#cc").text(cc);
   })
 
   $(".lckdn").click(function() {
     lck = subtract(lck);
     console.log(lck);
+    cc = calcCC();
     $("#lck").text(lck);
+    $("#cc").text(cc)
   })
 });
